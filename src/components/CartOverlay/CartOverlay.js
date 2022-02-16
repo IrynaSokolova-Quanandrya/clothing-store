229f730a8c778995615aab322205c51715b1c9a5
Iryna <irasokolova25@ukr.net>
2022-02-17 00:10:09 +0200

    add styles cart overlay

--- src/components/CartOverlay/CartOverlay.js
+++ src/components/CartOverlay/CartOverlay.js
@@ -7,30 +7,52 @@ export default class CartOverlay extends Component{
         return(
             <div className={s.backdrop}>
             <div className={s.cartOverlay}>
-                <p className={s.cartOverlay__title}>My Bag, Total items</p>
+                <p className={s.cartOverlay__title}>My Bag, <span className={s.cartOverlay__amount}>Total items</span></p>
                 <ul className={s.cartOverlay__list}>
                     <li className={s.cartOverlay__item}>
-                        <p>Product name</p>
-                        <p>$price</p>
-                        <form>
-                            <fieldset>
-                                <input type="radio" htmlFor="sizeChoice1" name="size" value="m"/>
-                                <label htmlFor="sizeChoice1">M</label>
-                                <input type="radio" htmlFor="sizeChoice2" name="size" value='s'/>
-                                <label htmlFor="sizeChoice2">S</label>
-                            </fieldset>
-                        </form>
+                        <div>
+                            <p className={s.cartOverlay__prod}>Apollo Running Short</p>
+                            <p className={s.cartOverlay__price}>$price</p>
+                            <form>
+                                    <input className={s.custom__checkbox} type="radio" htmlFor="sizeChoice1" name="size" value="m"/>
+                                    <label htmlFor="sizeChoice1"/>
+                                    <input className={s.custom__checkbox} type="radio" htmlFor="sizeChoice2" name="size" value='s'/>
+                                    <label htmlFor="sizeChoice2"/>
+                            </form>
+                        </div>
                         <div className={s.counter}>
                             <button type="button" className={s.counter__btn}>+</button>
                                 <span className={s.counter__value}>1</span>
                             <button type="button" className={s.counter__btn}>-</button>
                         </div>
-                        <img className={s.cartOverlay__img} src={image} alt='Foto'/> 
+                        <div>
+                            <img className={s.cartOverlay__img} src={image} alt='Foto'/> 
+                        </div>
+                    </li>
+                    <li className={s.cartOverlay__item}>
+                        <div>
+                            <p className={s.cartOverlay__prod}>Apollo Running Short</p>
+                            <p className={s.cartOverlay__price}>$price</p>
+                            <form>
+                                    <input className={s.custom__checkbox} type="radio" htmlFor="sizeChoice1" name="size" value="m"/>
+                                    <label htmlFor="sizeChoice1"/>
+                                    <input className={s.custom__checkbox} type="radio" htmlFor="sizeChoice2" name="size" value='s'/>
+                                    <label htmlFor="sizeChoice2"/>
+                            </form>
+                        </div>
+                        <div className={s.counter}>
+                            <button type="button" className={s.counter__btn}>+</button>
+                                <span className={s.counter__value}>1</span>
+                            <button type="button" className={s.counter__btn}>-</button>
+                        </div>
+                        <div>
+                            <img className={s.cartOverlay__img} src={image} alt='Foto'/> 
+                        </div>
                     </li>
                 </ul>
                 <div className={s.cartOverlay__total}>
-                    <p>Total</p>
-                    <p>$price</p>
+                    <p className={s.total}>Total</p>
+                    <p className={s.price}>$price</p>
                 </div>
                 <div className={s.cartOverlay__btn}>
                     <button type="button" className={s.btn}>VIEW BAG</button>
